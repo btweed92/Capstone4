ffca3c403c43f545736acccd5d044464732991e8
Brandyn Tweedly <tweedly92@gmail.com>
2019-08-26 08:51:46 -0400

    First Commit

---
+++ Capstone4/Controllers/TaskListController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone4.Controllers
+{
+    public class TaskListController : Controller
+    {
+        private readonly TaskListDbContext _context;
+
+        public TaskListController(TaskListDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(TaskList newTask)
+        {
+            AspNetUsers thisUser = _context.AspNetUsers.Where(u => u.UserName == User.Identity.Name).First();
+            newTask.Desciption = thisUser.Id;
+
+            
+            _context.TaskList.Add(newTask);
+            _context.SaveChanges();
+            return RedirectToAction("TaskList");
+        }
+        public IActionResult TaskList()
+        {
+            List<TaskList> userTasks = _context.TaskList.Find();
+            return View();
+        }
+    }
+}
---
+++ Capstone4/Models/TaskList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone4.Models
+{
+    public partial class TaskList
+    {
+        public int UserId { get; set; }
+        public string Desciption { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Complete { get; set; }
+    }
+}
